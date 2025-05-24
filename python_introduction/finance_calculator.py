8c267a60943e7984598b5ea890e95bd58a03a4bd
mihret kee <1keemem1@gmail.com>
2025-05-24 16:45:42 +0300

    create a monthly income interest

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,5 +5,5 @@ print(monthly_savings)
 projected_savings =int(monthly_savings) * 12 + int((monthly_savings) * 12 * 0.05)
 print(saving)
 print(f"The user's monthly savings is ${monthly_savings}")
-print(f"The user's annual saving is ${saving}")
+print(f"The user's annual saving is ${projected_savings}")
 
