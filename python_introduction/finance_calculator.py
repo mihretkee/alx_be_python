daa41e26da0957a6dff634831f60284706651918
mihret kee <1keemem1@gmail.com>
2025-05-24 16:55:19 +0300

    create a monthly income interest

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 monthly_income = float(input("enter your monthly income: "))
 monthly_expenses = float(input("enter your total monthly expense: "))
-monthly_savings = int(monthly_income) - int(monthly_expenses)
-print(monthly_savings)
-projected_savings =int((monthly_savings) * 12) + int((monthly_savings) * 12 * 0.05)
-print(saving)
-print(f"Your monthly savings are ${monthly_savings}")
-print(f"Projected savings after one year, with interest, is:${projected_savings}")
+
+monthly_savings = monthly_income - monthly_expenses
+projected_savings =(monthly_savings * 12) + (monthly_savings * 12 * 0.05)
+
+print(f"Your monthly savings are ${monthly_savings:.0f}")
+print(f"Projected savings after one year, with interest, is:${projected_savings:.0f}")
 
