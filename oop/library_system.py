1110a8c446d12dfa7db3fae7802615f1aa7b84f8
mihret kee <1keemem1@gmail.com>
2025-06-20 17:54:25 +0300

    create inheritance and composition in python

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,9 +2,12 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
-    class EBook(Book):
+
+
+class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
@@ -20,9 +23,11 @@ class PrintBook(Book):
 
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-    class Library:
+
+
+class Library:
     def __init__(self):
-        self.books = []  # composition: list of book objects
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
@@ -30,6 +35,4 @@ class PrintBook(Book):
     def list_books(self):
         for book in self.books:
             print(book)
-
-
-    
+            
