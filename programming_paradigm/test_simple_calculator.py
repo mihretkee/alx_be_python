ce256bf640fcdcd93695f42ff6a770bae364827a
mihret kee <1keemem1@gmail.com>
2025-06-20 16:20:59 +0300

    createpython magic methods

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+from programming_paradigm.simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertIsNone(self.calc.divide(5, 0))  # Divide by zero case
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python_introduction/python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print(sum)
+diff = number1 - number2
+print(diff)
+pro = number1 * number2
+print(pro)
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"substraction of {number1} and {number2} is {diff}")
+print(f"multiplication of {number1} and {number2} is {pro}")
